5b5e3ee805b84d45dfb4786c3bfb78dcb5ff1a2f
KodaEd <edrickkoda@gmail.com>
2026-02-04 00:53:59 +1100

    implemented scraper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+
+
+mod app;
+use app::App;
+
+fn main() -> () {
+    let mut app = App::new();
+    app.get_ritz_movies();
+
+    println!("{:?}", app.ritz_movie_times);
 }
